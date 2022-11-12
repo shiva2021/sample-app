7c4f233753b010e53e00ea59a2ed4842bf9bfb19
shiva2021 <shiva.chaturvedi91@gmail.com>
2022-11-12 17:32:07 +0530

    basic project setup

---
+++ lambdaFn/createFile.js
@@ -0,0 +1,4 @@
+import { S3 } from "aws-sdk";
+import * as fs from "fs";
+
+(async function () {})();
--- lib/sample-app-stack.ts
+++ lib/sample-app-stack.ts
@@ -1,5 +1,12 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import {
+  CfnOutput,
+  aws_s3 as s3,
+  aws_dynamodb as db,
+  aws_lambda as lambda,
+  aws_lambda_event_sources as eventSources,
+} from "aws-cdk-lib";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class SampleAppStack extends cdk.Stack {
